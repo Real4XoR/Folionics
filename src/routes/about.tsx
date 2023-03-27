d371485a27ab7d33c52382c6fc22dacd846f930c
Nathan Cobbald <nathan.cobbald@zxsecurity.co.nz>
2023-03-28 10:42:30 +1300

    Tests

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,17 +1,5 @@
 import { Title } from "solid-start";
 
 export default function Home() {
-    return (
-      <main>
-        <Title>About</Title>
-        <h1>Welcome, take a look around</h1>
-        <p>
-          Visit{" "}
-          <a href="https://github.com/Real4XoR" target="_blank">
-            GitHub
-          </a>
-        to check out my other projects
-        </p>
-      </main>
-    );
-  }
+  <p>About page</p>
+}
