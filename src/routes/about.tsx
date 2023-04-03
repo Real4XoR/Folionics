08457955ed810c7599543abe5b152f0d4a63380e
Nathan Cobbald <cobbaldn@gmail.com>
2023-04-03 17:43:07 +1200

    Update About

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -5,6 +5,14 @@ export default function Home() {
     <main>
       <Title>About</Title>
       <h1>About page</h1>
+
+      <p>
+        Click{" "}
+        <a href="https://nathancobbald.tech" target="_blank">
+          Here
+        </a>
+      to return home 
+      </p>
     </main>
   );
 }
